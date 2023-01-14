89aa012da7c223ee4bb2a80c9e65bd0a60fb4255
HippopoStar <leocabanes@wanadoo.fr>
2023-01-14 18:45:20 +0100

    MaJ "generate_citra_cheat_codes.sh"

--- scripts/generate_citra_cheat_codes.sh
+++ scripts/generate_citra_cheat_codes.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( realpath --zero "${0}" | xargs --null dirname )"
 RESOURCES_DIR="$( dirname "${SCRIPT_DIR}" )/resources"
 
-cp "${RESOURCES_DIR}/0004000000126300" ~/.var/app/org.citra_emu.citra/data/citra-emu/cheats/
+cp "${RESOURCES_DIR}/0004000000126100.txt" ~/.var/app/org.citra_emu.citra/data/citra-emu/cheats/
