f6de51a58de8ecca8b2fbaaf028571a615061ae3
HippopoStar <leocabanes@wanadoo.fr>
2023-01-14 18:06:26 +0100

    Ajout de "generate_citra_cheat_codes.sh"

---
+++ scripts/generate_citra_cheat_codes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( realpath --zero "${0}" | xargs --null dirname )"
+RESOURCES_DIR="$( dirname "${SCRIPT_DIR}" )/resources"
+
+cp "${RESOURCES_DIR}/0004000000126300" ~/.var/app/org.citra_emu.citra/data/citra-emu/cheats/
